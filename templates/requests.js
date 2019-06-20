327ad9bf9b26ab11982e7ae90fcb4d0633c8eceb
J Hill <chirolhi@usc.edu>
2019-06-19 21:25:43 -0700

    added script to access backend api

---
+++ templates/requests.js
@@ -0,0 +1,37 @@
+function getRide(rideId) {
+  $.ajax({
+    url: '/getRide',
+    method: 'POST',
+    data: {
+      'rideId':
+    }
+  }).done(function(result) {
+    console.log(result);
+  }).fail(function() {
+    console.log('AJAX error');
+  });
+}
+
+function getAllRides() {
+  $.ajax({
+    url: '/getRides',
+    method: 'POST',
+    data: {}
+  }).done(function(result) {
+    console.log(result);
+  }).fail(function() {
+    console.log('AJAX error');
+  });
+}
+
+function getAllRequests() {
+  $.ajax({
+    url: '/getRequests',
+    method: 'POST',
+    data: {}
+  }).done(function(result) {
+    console.log(result);
+  }).fail(function() {
+    console.log('AJAX error');
+  });
+}
